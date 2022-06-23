e1a42a75241d25f72474829e2b903eef7f2bc059
SviatoslavZenkevich <sviatoslav.zenkevich@gmail.com>
2022-06-23 13:02:19 +0300

    Add

--- src/main/java/by/academy/classwork/lesson4/Zadacha4.java
+++ src/main/java/by/academy/classwork/lesson4/Zadacha4.java
@@ -2,29 +2,34 @@ package by.academy.classwork.lesson4;
 
 import java.util.Scanner;
 
-//public class Zadacha4 {
-//    public static void main(String[] args) {
-//
-//        int polozhitChisla = 1;
-//        int otrizatChisla;
-//        int Nuli;
-//
-//
-//        Scanner scan = new Scanner(System.in);
-//        System.out.println("Enter number of integers: ");
-//        int n = scan.nextInt();
-//
-//        for (int i = 1; i < n; i++) {
-//            System.out.println("Your number " + i + ": ");
-//            int num = scan.nextInt();
-//
-//            if (num > 0) {
-//                polozhitChisla++;
-//
-//            }
-//            System.out.println("Your number " + i + ": ");
-//            int num = scan.nextInt();
-//            scan.close();
-//        }
-//
-//    }
+public class Zadacha4 {
+    public static void main(String[] args) {
+
+        int polozhitChisla=0;
+        int otrizatChisla=0;
+        int nuli=0;
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number of integers: ");
+        int n = scan.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Your number " + i + ": ");
+            int num = scan.nextInt();
+
+            if (num > 0) {
+                polozhitChisla++;
+            } else if (num < 0) {
+                otrizatChisla++;
+            } else
+                nuli++;
+        }
+        System.out.println("Kolichestvo polozhitelnih chisel ravno "+ polozhitChisla);
+        System.out.println("Kolichestvo otricatelnih chisel ravno "+ otrizatChisla);
+        System.out.println("Kolichestvo nulej ravno "+ nuli);
+        scan.close();
+        }
+
+    }
+
